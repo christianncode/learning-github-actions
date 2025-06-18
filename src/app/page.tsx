7865c9733a55da8d595945e8411a5ba0f012ecc3
chrisdca <chrisanderdev@gmail.com>
2025-06-17 23:26:45 -0500

    update action version in workflow and homepage heading to v1.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>My first deploy with Github Actions and use my own action v1.1</h1>
+        <h1>My first deploy with Github Actions and use my own action v1.2</h1>
       </main>
     </div>
   );
